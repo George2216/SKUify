3dc863e77e76088b8a058ac9d0e4dcccf534839f
georgechurikov <georgechurikov@gmail.com>
2023-11-28 13:57:55 +0200

    add SalesNavigator

---
+++ SKUify/Scenes/MainFlow/SalesScene/SalesVC.swift
@@ -0,0 +1,16 @@
+//
+//  SalesVC.swift
+//  SKUify
+//
+//  Created by George Churikov on 28.11.2023.
+//
+
+import Foundation
+
+final class SalesNavigator: BaseViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+}
