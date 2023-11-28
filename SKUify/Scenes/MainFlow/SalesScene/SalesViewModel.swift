d1dd3ae75274cb46bf15cd43dfc06cd27d9449b7
georgechurikov <georgechurikov@gmail.com>
2023-11-28 13:59:17 +0200

    add SalesViewModel

---
+++ SKUify/Scenes/MainFlow/SalesScene/SalesViewModel.swift
@@ -0,0 +1,34 @@
+//
+//  SalesViewModel.swift
+//  SKUify
+//
+//  Created by George Churikov on 28.11.2023.
+//
+
+import Foundation
+import Domain
+import RxSwift
+import RxCocoa
+import RxExtensions
+
+import Foundation
+import RxSwift
+
+final class SalesViewModel: ViewModelProtocol {
+    
+    func transform(_ input: Input) -> Output {
+        return Output()
+    }
+    
+    struct Input {
+        
+    }
+    
+    struct Output {
+        
+    }
+    
+}
+
+
+
