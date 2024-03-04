3882839d7a43fa26ab0957e60e26d184217e1532
georgechurikov <georgechurikov@gmail.com>
2024-03-04 16:55:41 +0200

    Add marketplaces popover

---
+++ SKUify/Scenes/Popovers/MarketplacesPopoverVC/MarketplacesPopoverTV.swift
@@ -0,0 +1,20 @@
+//
+//  MarketplacesPopoverTV.swift
+//  SKUify
+//
+//  Created by George Churikov on 04.03.2024.
+//
+
+import UIKit
+
+class MarketplacesPopoverTV: UITableView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
---
+++ SKUify/Scenes/Popovers/MarketplacesPopoverVC/MarketplacesPopoverTVCell.swift
@@ -0,0 +1,23 @@
+//
+//  MarketplacesPopoverTVCellTableViewCell.swift
+//  SKUify
+//
+//  Created by George Churikov on 04.03.2024.
+//
+
+import UIKit
+
+class MarketplacesPopoverTVCellTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
